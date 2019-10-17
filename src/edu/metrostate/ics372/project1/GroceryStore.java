318f76aad075aff61683d7647d0f27dd97e37a4a
dnguyen000 <don.nguyen.000@gmail.com>
2019-10-17 16:55:04 -0500

    adding interface, class, and test files for Product

--- src/edu/metrostate/ics372/project1/GroceryStore.java
+++ src/edu/metrostate/ics372/project1/GroceryStore.java
@@ -1,5 +1,17 @@
 package edu.metrostate.ics372.project1;
 
-public class GroceryStore {
+import java.util.HashMap;
+import java.util.Map;
 
+//TODO: create main collection class for all collections
+public class GroceryStore {
+	private Map<String, Product> inventory;
+	private Map<String, Product> cart;
+	private Map<String, Member> members;
+	
+	public GroceryStore() {
+		this.inventory = new HashMap<>();
+		this.cart = new HashMap<>();
+		this.members = new HashMap<>();
+	}
 }
